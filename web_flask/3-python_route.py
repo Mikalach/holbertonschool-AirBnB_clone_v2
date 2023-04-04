925a70aeb25ec24f0e3ffd5a61c954e38dacba1f
Mikalach <mika.djetic@gmaiul.com>
2023-04-03 19:31:13 -0700

    task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a script that starts a Flask web application """
+"""script that starts a Flask web application"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -27,11 +27,6 @@ def python(text):
     if "_" in text:
         text = text.replace("_", " ")
     return f"Python {text}"
-
-
-@app.route("/python", strict_slashes=False)
-def python_default():
-    return f"Python is cool"
 
 
 if __name__ == "__main__":
