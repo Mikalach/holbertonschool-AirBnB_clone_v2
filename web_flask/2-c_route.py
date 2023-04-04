3eb3f60d0de9ec08b89d5ba8196006b4146e3861
Mikalach <mika.djetic@gmaiul.com>
2023-04-03 19:27:27 -0700

    task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" a script that starts a Flask web application """
+"""a script that starts a Flask web applicationx"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,18 +9,15 @@ app = Flask(__name__)
 def hello_world():
     return "Hello HBNB!"
 
-
 @app.route("/hbnb", strict_slashes=False)
 def display():
     return "HBNB"
-
 
 @app.route("/c/<text>", strict_slashes=False)
 def c_is_fun(text):
     if "_" in text:
         text = text.replace("_", " ")
     return f"C {text}"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
