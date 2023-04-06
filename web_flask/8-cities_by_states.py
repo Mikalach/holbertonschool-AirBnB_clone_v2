c9bfb6caecfedb453350a59b5177909823418366
Mikalach <mika.djetic@gmaiul.com>
2023-04-06 06:41:09 -0700

    pep8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,10 +9,8 @@ from models.city import City
 app = Flask(__name__)
 
 
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    
     states = storage.all(State).values()
     cities = storage.all(City).values()
     return render_template('8-cities_by_states.html',
